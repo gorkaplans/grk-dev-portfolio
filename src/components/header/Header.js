d2a181f459c8be32bb781de514afba7f353a5d69
Gorka Plans <gorkaplans@gmail.com>
2021-08-26 14:18:14 +0200

    config the project

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React, { useState }from 'react'; 
+
+import'./Header.scss'
+
+
+
+
+const Header = () => {
+ 
+    return (
+        <h1 className="text-light">Header</h1>
+    
+    ) } 
+ 
+  export default Header;
