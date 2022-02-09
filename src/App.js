50d270b051938432b4e78248027f79b750f0c8fc
GorkaPlans <gorkaplans@gmail.com>
2022-02-09 21:21:29 +0100

    refact dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { ThemeProvider } from "styled-components";
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Abstract from "./components/abstract";
 import Projects from "./components/projects";
 import Footer from "./components/footer";
-import { Body } from "./components/Body";
 
-import Themes from "./components/theme/themes";
 import { useState, useEffect } from "react";
 
 export { default as Themes } from "./components/theme/themes";
@@ -22,21 +19,17 @@ function App() {
 
   return (
     <>
-      <ThemeProvider theme={Themes[theme]}>
-        <Body>
-          <div className="App">
-            <div className="w-full px-10">
-              <Header theme={theme} setTheme={setTheme}></Header>
-              <Hero></Hero>
-              <Abstract></Abstract>
-              <Projects></Projects>
-              <Footer></Footer>
-            </div>
-          </div>
-        </Body>
-      </ThemeProvider>
+      <div className="App" data-theme={theme}>
+        <div className="w-full px-10">
+          <Header theme={theme} setTheme={setTheme}></Header>
+          <Hero></Hero>
+          <Abstract></Abstract>
+          <Projects></Projects>
+          <Footer></Footer>
+        </div>
+      </div>
     </>
   );
 }
-
+  
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import'./Header.scss'
 import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 
 
-import ButtonStyled from '../Button/ButtonStyled'
+import Button from '../Button/Button';
 
 
 const Header = (props) => {
@@ -12,7 +12,7 @@ const Header = (props) => {
         <>
         <div className="mx-auto w-full flex items-center pt-8 pb-4 border-b border-solid border-current">
             <h1 className="text-light mr-auto">Gorka Plans</h1>
-             <ButtonStyled theme={props.theme} setTheme={props.setTheme} className="mr-4"></ButtonStyled>
+             <Button theme={props.theme} setTheme={props.setTheme} className="mr-4"></Button>
              {props.theme === 'light' ? <BsFillMoonFill /> : <BsFillSunFill /> } 
         </div>
         
